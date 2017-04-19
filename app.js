e681a2da0a3f49f34e5c32c75dfaa029b26a61c5
zhangxq <878773559@qq.com>
2017-04-19 11:22:26 +0800

    删除之前项目所有文件

--- app.js
+++
@@ -1,5 +0,0 @@
-var app = require("./lib/server");
-
-app.createWebApplication().listen(process.env.PORT || 4700, process.env.IP || "0.0.0.0", function(){
-  console.log("Chat server listening at");
-});
