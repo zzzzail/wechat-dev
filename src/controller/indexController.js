04c5d3fa1c7bd7e77e2f1e14b5658face5b9cca7
zhangxq <878773559@qq.com>
2017-04-20 17:35:56 +0800

    微信配置修改到wechat.js

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -9,26 +9,5 @@ const sha1 = require('sha1');
 const config = require('../config/config');
 
 exports.getIndex = async (ctx, next) => {
-  const query = ctx.query;
-  let token = config.wechat.token;
-  let signature = query.signature;
-  let echostr = query.echostr;
-  let timestamp = query.timestamp;
-  let nonce = query.nonce;
-  if (signature && echostr && timestamp && nonce) {
-    let str = [token, timestamp, nonce].sort().join('');
-    let sha = sha1(str);
-    if (sha == signature) {
-      return ctx.body = echostr;
-    }
-  }
-
-  const {
-    getAccessToken,
-    getJsapiTicket,
-    createSign
-  } = ctx.wechat;
-  const tokenData = await getAccessToken();
-
   return ctx.render('index');
 }
--- src/middleware/wechat.js
+++ src/middleware/wechat.js
@@ -5,15 +5,36 @@
  * @description
  */
 
+const sha1 = require('sha1');
 const Wechat = require('../wechat');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   const wechat = new Wechat();
 
-  app.use(async(ctx, next) => {
+  app.use(async (ctx, next) => {
     ctx.wechat = wechat;
     await next();
   });
+
+  app.use(async (ctx, next) => {
+    const query = ctx.query;
+    let token = config.wechat.token;
+    let signature = query.signature;
+    let echostr = query.echostr;
+    let timestamp = query.timestamp;
+    let nonce = query.nonce;
+    if (signature && echostr && timestamp && nonce) {
+      let str = [token, timestamp, nonce].sort().join('');
+      let sha = sha1(str);
+      if (sha == signature) {
+        return ctx.body = echostr;
+      } else {
+        return ctx.body = '不要碰爸爸的微信公众号.🙂';
+      }
+    } else {
+      await next();
+    }
+  })
 
 }
