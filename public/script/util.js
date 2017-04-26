c9bf73636df7fccfae273d5e26fe13af91146aa4
zhangxq <878773559@qq.com>
2017-04-26 14:42:20 +0800

    添加querystring, 修改细节, 上传微信素材成功

--- public/script/util.js
+++ public/script/util.js
@@ -13,4 +13,16 @@ Util.__proto__.getFileSize = function (id) {
   return (ele.files[0].size / 1024).toFixed(2);
 }
 
+// 解析url中带的参数
+Util.__proto__.QueryString = function (name, url) {
+  var url = url || window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  var results = regex.exec(url);
+  console.log(results)
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 window.Util = Util;
