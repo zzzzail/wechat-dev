ce0694687fe9372d9d8c7e1727e7a7b02865dfbc
zhangxq <878773559@qq.com>
2017-05-18 17:51:55 +0800

    登录hidden信息

--- src/controller/wechat/indexController.js
+++ src/controller/wechat/indexController.js
@@ -23,11 +23,27 @@ exports.getLogin = async (ctx, next) => {
   if(!code || !state) {
     return ctx.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wechatCfg.appId}&redirect_uri=${ctx.href}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`)
   }
-  
   let siteAccessToken = await ctx.wechat.getSiteAccessToken(code);
-  console.log(siteAccessToken);
   let userInfo = await ctx.wechat.getUserInfo(siteAccessToken.access_token, siteAccessToken.openid);
-  console.log(userInfo);
-  
-  return ctx.render('wechat/login');
+  let openid = userInfo.openid; // 唯一标示
+  let nickname = userInfo.nickname; // 昵称
+  let sex = userInfo.sex; // 性别, 值为1时是男性,值为2时是女性,值为0时是未知
+  let country = userInfo.country; // 国家
+  let province = userInfo.province; // 省份
+  let city = userInfo.city; // 城市
+  let headimgurl = userInfo.headimgurl; // 用户头像
+  let privilege = userInfo.privilege; // 用户特权信息，json 数组，如微信沃卡用户为（chinaunicom）
+  let unionid = userInfo.unionid; // 只有用户绑定到微信开放平台账号后,才会出现该字段.
+
+  return ctx.render('wechat/login', {
+    openid,
+    nickname,
+    sex,
+    country,
+    province,
+    city,
+    headimgurl,
+    privilege: privilege.toString(),
+    unionid
+  });
 }
